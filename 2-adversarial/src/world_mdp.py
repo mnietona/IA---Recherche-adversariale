9cd5c272d740212c89ed79860c25e7a4e58bca29
matias <matiasnieto1419@gmail.com>
2023-10-11 17:56:26 +0200

    world mdp ok

--- 2-adversarial/src/world_mdp.py
+++ 2-adversarial/src/world_mdp.py
@@ -1,33 +1,45 @@
 from dataclasses import dataclass
 import lle
-from lle import World, Action
+from lle import World, Action, WorldState
 from mdp import MDP, State
-
+from typing import List
 
 @dataclass
 class MyWorldState(State):
-    ...
+    world: World
+    world_state: WorldState
+    current_agent: int
+    value: float
 
+    def is_final(self):
+        return self.world.done
 
-class WorldMDP(MDP[Action, MyWorldState]):
+class WorldMDP(MDP[MyWorldState, Action]):
     def __init__(self, world: World):
         self.world = world
 
-    def reset(self):
-        self.n_expanded_states = 0
-        ...
+    def reset(self) -> MyWorldState:
+        self.world.reset()
+        return MyWorldState(world=self.world, world_state=self.world.get_state(), current_agent=0, value=0)
 
-    def available_actions(self, state: MyWorldState) -> list[Action]:
-        ...
+    def transition(self, state: MyWorldState, action: Action) -> MyWorldState:
+        if self.world.agents[state.current_agent].is_alive:
+            # Seul l'agent actif agit, les autres effectuent l'action STAY.
+            actions = [Action.STAY] * self.world.n_agents
+            actions[state.current_agent] = action
+            reward = self.world.step(actions)
+            new_value = state.value + reward if state.current_agent == 0 else state.value
+            if self.world.agents[0].is_dead:
+                new_value = lle.REWARD_AGENT_DIED
+        else:
+            new_value = state.value
+
+        next_agent = (state.current_agent + 1) % self.world.n_agents
+        return MyWorldState(world=self.world,world_state=self.world.get_state(), current_agent=next_agent, value=new_value)
+
+    def available_actions(self, state: MyWorldState) -> List[Action]:
+        actions_for_all_agents = self.world.available_actions()
+        return actions_for_all_agents[state.current_agent]
 
     def is_final(self, state: MyWorldState) -> bool:
-        ...
-
-    def transition(self, state: MyWorldState, action: Action) -> MyWorldState:
-        ...
-
-
-class BetterValueFunction(WorldMDP):
-    def transition(self, state: MyWorldState, action: Action) -> MyWorldState:
-        # Change the value of the state here.
-        ...
+        return state.is_final()
