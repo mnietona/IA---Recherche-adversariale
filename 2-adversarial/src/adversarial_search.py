f9da06548a319655c73648af308add72bdd82baf
matias <matiasnieto1419@gmail.com>
2023-10-16 14:42:29 +0200

    minimax ok

--- 2-adversarial/src/adversarial_search.py
+++ 2-adversarial/src/adversarial_search.py
@@ -1,9 +1,48 @@
 from lle import Action
 from mdp import MDP, S, A
-
+from typing import Tuple
 
 def minimax(mdp: MDP[A, S], state: S, max_depth: int) -> A:
-    ...
+    """ Return the best action according to the minimax algorithm."""
+    if state.current_agent != 0:
+        raise ValueError("The state should be for agent 0 to play.")
+
+    
+    # Wrapper to maximize value and action
+    def _max(state: S, depth: int) -> Tuple[float, A]:
+        if mdp.is_final(state) or depth == max_depth:
+            return state.value, None
+        value = float('-inf')
+        best_action = None
+        for action in mdp.available_actions(state):
+            child = mdp.transition(state, action)
+            # Check if next state has a different agent type (0 vs non-0)
+            decrease_depth = 1 if child.current_agent != state.current_agent else 0
+            child_value, _ = _min(child, depth + decrease_depth)
+            if child_value > value:
+                value, best_action = child_value, action
+
+        return value, best_action
+
+    # Wrapper to minimize value for the adversaries
+    def _min(state: S, depth: int) -> Tuple[float, A]:
+        if mdp.is_final(state) or depth == max_depth:
+            return state.value, None
+        value = float('inf')
+        for action in mdp.available_actions(state):
+            child = mdp.transition(state, action)
+            # Check if next state has a different agent type (0 vs non-0)
+            decrease_depth = 1 if child.current_agent == 0 else 0
+            if child.current_agent == 0:  # if it's agent 0's turn
+                child_value, _ = _max(child, depth + decrease_depth)
+            else:  # if it's another agent's turn
+                child_value, _ = _min(child, depth + decrease_depth)
+            value = min(value, child_value)
+
+        return value, None
+
+    _, best_action = _max(state, 0)
+    return best_action
 
 
 def alpha_beta(mdp: MDP[A, S], state: S, max_depth: int) -> A:
--- 2-adversarial/src/world_mdp.py
+++ 2-adversarial/src/world_mdp.py
@@ -25,21 +25,26 @@ class WorldMDP(MDP[MyWorldState, Action]):
         return MyWorldState(world=self.world, world_state=self.world.get_state(), current_agent=0, value=0)
 
     def transition(self, state: MyWorldState, action: Action) -> MyWorldState:
-
-        if self.world.agents[state.current_agent].is_alive and not self.world.done:
-            # Seul l'agent actif agit, les autres effectuent l'action STAY.
-            actions = [Action.STAY] * self.world.n_agents
-            actions[state.current_agent] = action
-            reward = self.world.step(actions)
-            new_value = state.value + reward if state.current_agent == 0 else state.value
+        
+        self.world.set_state(state.world_state)
+        actions_for_all_agents = [Action.STAY] * self.world.n_agents
+        actions_for_all_agents[state.current_agent] = action
+        reward = self.world.step(actions_for_all_agents)
+        
+        if state.current_agent == 0: 
             if self.world.agents[0].is_dead:
                 new_value = lle.REWARD_AGENT_DIED
+            else:
+                new_value = state.value + reward
         else:
             new_value = state.value
 
         next_agent = (state.current_agent + 1) % self.world.n_agents
-        return MyWorldState(world=self.world,world_state=self.world.get_state(), current_agent=next_agent, value=new_value)
+        
+        new_state = MyWorldState(world=self.world, world_state=self.world.get_state(), current_agent=next_agent, value=new_value)
+        return new_state
 
+    
     def available_actions(self, state: MyWorldState) -> List[Action]:
         actions_for_all_agents = self.world.available_actions()
         return actions_for_all_agents[state.current_agent]
